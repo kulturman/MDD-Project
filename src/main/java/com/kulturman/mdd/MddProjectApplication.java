06423a9edf880d5e17fcb6c27b3a5b02ff1df53f
kulturman <arnaudbakyono@gmail.com>
2023-06-21 20:56:17 +0000

    Init backend project

---
+++ src/main/java/com/kulturman/mdd/MddProjectApplication.java
@@ -0,0 +1,13 @@
+package com.kulturman.mdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MddProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MddProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kulturman/mdd/MddProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kulturman.mdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MddProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
