d2bc912b8301a87b9c3d0284b84bdd42dcd99399
kulturman <arnaudbakyono@gmail.com>
2023-06-23 17:39:47 +0000

    Use input values for article component

--- front/src/app/pages/articles/articles.component.ts
+++ front/src/app/pages/articles/articles.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from '@angular/core';
+import {Component} from '@angular/core';
 import {Router} from "@angular/router";
 
 @Component({
@@ -7,10 +7,6 @@ import {Router} from "@angular/router";
   styleUrls: ['./articles.component.css']
 })
 export class ArticlesComponent {
-  @Input() title!: string;
-  @Input() date!: string;
-  @Input() author!: string;
-
   constructor(private readonly router: Router) {
   }
   async navigate() {
