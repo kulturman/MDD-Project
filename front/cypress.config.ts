d9054ee0619a2025d8d0886d61e834cc994d64f5
kulturman <arnaudbakyono@gmail.com>
2023-10-06 19:37:25 +0000

    Add E2E tests for login

---
+++ front/cypress.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  component: {
+    devServer: {
+      framework: "angular",
+      bundler: "webpack",
+    },
+    specPattern: "**/*.cy.ts",
+  },
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    env: {
+      development: {
+        baseUrl: "http://localhost:4200"
+      }
+    }
+  },
+});
---
+++ front/cypress/e2e/login_spec.cy.ts
@@ -0,0 +1,23 @@
+describe('login', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:4200/login');
+  })
+
+  it('deactivates submit button if form is empty', () => {
+    cy.get('[data-cy=login-button]').should('have.attr', 'disabled');
+  })
+
+  it('displays dialog if credentials are wrong', () => {
+    cy.get('[id=email]').type("itachi@gmail.com");
+    cy.get('[id=password]').type("konoha");
+    cy.get('[data-cy=login-button]').click();
+    cy.get('.error-dialog').should('be.visible');
+  })
+
+  it('redirects to home on login success', () => {
+    cy.get('[id=email]').type("itachi@konoha.com");
+    cy.get('[id=password]').type("Aa123456@");
+    cy.get('[data-cy=login-button]').click();
+    cy.url().should('contain', 'articles')
+  })
+})
